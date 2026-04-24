8e2d92b0a2a97c81d29bcd0e42dd129ebbb8e49b
youyou <15814878035@163.com>
2026-04-24 16:25:09 +0800

    修复fixes/issues_1.txt

--- scripts/build.bat
+++ scripts/build.bat
@@ -16,10 +16,30 @@ if defined VSDEVCMD (
     echo WARNING: Visual Studio not found, trying default compiler
 )
 
+:: Detect OpenSSL for HTTPS support
+set OPENSSL_ROOT_DIR=%OPENSSL_ROOT_DIR%
+if "%OPENSSL_ROOT_DIR%"=="" (
+    for %%d in (
+        "C:\Users\user\anaconda3\Library"
+        "C:\Program Files\OpenSSL-Win64"
+        "C:\Program Files\OpenSSL"
+        "C:\vcpkg\packages\openssl_x64-windows"
+    ) do (
+        if exist "%%~d\include\openssl\ssl.h" (
+            if not defined OPENSSL_ROOT_DIR set "OPENSSL_ROOT_DIR=%%~d"
+        )
+    )
+)
+if not "%OPENSSL_ROOT_DIR%"=="" (
+    echo Using OpenSSL: %OPENSSL_ROOT_DIR%
+) else (
+    echo WARNING: OpenSSL not found, HTTPS support may be disabled
+)
+
 if not exist build mkdir build
 cd build
 
-cmake .. -DBUILD_TESTS=ON
+cmake .. -DBUILD_TESTS=ON -DOPENSSL_ROOT_DIR="%OPENSSL_ROOT_DIR%"
 if errorlevel 1 goto :error
 
 cmake --build . --config %BUILD_TYPE% --parallel
