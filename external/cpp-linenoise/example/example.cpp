cac72f033e06ed0ac5847ab9610309d4aa215a4b
youyou <15814878035@163.com>
2026-04-25 09:48:55 +0800

    add external libs

---
+++ external/cpp-linenoise/example/example.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "../linenoise.hpp"
+
+using namespace std;
+
+int main(int argc, const char** argv)
+{
+    const auto path = "history.txt";
+
+#ifdef _WIN32
+    const char *prompt = "hello> ";
+#else
+    const char *prompt = "\033[32mこんにちは\x1b[0m> ";
+#endif
+
+    linenoise::linenoiseState l(prompt);
+
+    // Enable the multi-line mode
+    l.EnableMultiLine();
+
+    // Set max length of the history
+    l.SetHistoryMaxLen(4);
+
+    // Setup completion words every time when a user types
+    l.SetCompletionCallback([](const char* editBuffer, std::vector<std::string>& completions) {
+        if (editBuffer[0] == 'h') {
+#ifdef _WIN32
+            completions.push_back("hello こんにちは");
+            completions.push_back("hello こんにちは there");
+#else
+            completions.push_back("hello");
+            completions.push_back("hello there");
+#endif
+        }
+    });
+
+    // Load history
+    l.LoadHistory(path);
+
+    while (true) {
+        std::string line;
+        auto quit = l.Readline(line);
+
+        if (quit) {
+            break;
+        }
+
+        cout <<  "echo: '" << line << "'" << endl;
+
+        // Add line to history
+        l.AddHistory(line.c_str());
+
+        // Save history
+        l.SaveHistory(path);
+    }
+
+    return 0;
+}
